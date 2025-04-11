7b6e7035e97f920cba2fa5d1239b12eee8e6be76
Balofoide <viniciuschavito@gmail.com>
2025-04-11 12:13:39 -0300

    Exit function

--- src/utils/menu/menu.rs
+++ src/utils/menu/menu.rs
@@ -7,9 +7,15 @@ use crate::utils::servers::server_tcp::{user_input,handle_clients,manage_alias};
 use crate::utils::payload::tcp_generator::gen_tcp;
 
 pub fn spawn_menu(database:Arc<Database>){
+ 
     loop{
         let db_clone = Arc::clone(&database);
-        interprete(db_clone);  
+        let keep_alive = interprete(db_clone); 
+
+        if !keep_alive{
+            break;
+        }
+         
     }
 }
 
@@ -36,7 +42,7 @@ pub fn banner(){
 }
 
 
-pub fn interprete(database: Arc<Database>){
+pub fn interprete(database: Arc<Database>) -> bool{
 
     print!("\n>");
     io::stdout().flush().expect("Falha ao fazer flush do stdout");
@@ -45,26 +51,31 @@ pub fn interprete(database: Arc<Database>){
     let comandos:Vec<&str> = input.split_whitespace().collect();
 
     if comandos.is_empty(){
-        return;
+        return true;
     }
 
     match comandos[0].to_lowercase().as_str(){
         "connect" => {
             if comandos.len() != 2 {
                 println!("Comando Incorreto.");
+                return true;
             }else {
                 handle_clients(database, comandos[1]);
+                return true;
             }
         }
         "alias" => {
             if comandos.len() != 3 {
                 println!("Comando Incorreto.");
+                return true;
             }else{
                  manage_alias(database, comandos[1], comandos[2]);
+                 return true;
             }
         }
         "list" => {
             database.list_clientes();
+            return true;
         }
         "help" => {
             println!("------Commands------");
@@ -75,15 +86,25 @@ pub fn interprete(database: Arc<Database>){
             println!("help");
             println!("banner");
             println!("gen");
-            
+            println!("exit");
+            return true;
         }
         "clear" => {
             std::process::Command::new("clear").status().unwrap();
+            return true;
         }
         "gen" => {
             gen_tcp(comandos[1], comandos[2]);
+            return true;
         }
-        _ => println!("Comando inexistente")
+        "banner" => {
+            banner();
+            return true;
+        }
+        "exit" => {
+            return false;
+        }
+        _ => {println!("Comando inexistente");return true;}
     }
 
 }
